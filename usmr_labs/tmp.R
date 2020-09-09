981abc9db04ae83139fb057b49f882fcf2bd76e4
Josiah King <josiahpjking@gmail.com>
2020-09-09 09:37:53 +0100

    labs update ready for week 1

---
+++ usmr_labs/tmp.R
@@ -0,0 +1,8 @@
+params <-
+list(SHOW_SOLS = TRUE, TOGGLE = TRUE)
+
+## ----setup, include=FALSE---------------------------------------------------------------------------
+source('assets/setup.R')
+library(tidyverse)
+library(patchwork)
+
