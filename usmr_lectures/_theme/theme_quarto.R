cb420d7256aa05efeae43113bb92e2095c936981
Martin Corley <Martin.Corley@ed.ac.uk>
2023-07-21 17:46:07 +0100

    progress on l1

--- usmr_lectures/_theme/theme_quarto.R
+++ usmr_lectures/_theme/theme_quarto.R
@@ -53,6 +53,7 @@ options(digits=4,scipen=8)
 
 knitr::opts_chunk$set(
   tidy=TRUE,
+  tidy.opts=list(width.cutoff=70, arrow=TRUE, indent=2, args.newline=TRUE),
   dev="svg"
 )
 knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
@@ -61,3 +62,47 @@ color_text <- '#000000'
 color_bg <- '#ffffff'
 font_title <- 'Bakbak One'
 theme_set(theme_quarto())
+
+### my functions
+
+.pp <- function(command,top=3,bottom=-3,l=FALSE) {
+  t <- capture.output(eval(command))
+  ln <- length(t)
+  if (class(l)!='logical') {
+    i=0
+    for (n in l) {
+      if (i>0) {
+        cat("...",sep="\n")
+      }
+      i=i+1
+      if (length(n)==1 && (i == 1 || i == length(l))) {
+        if (n<0) {
+          s <- n+1+ln
+          n <- c(s:ln)
+        } else {
+          n <- c(1:n)
+        }
+      }
+      if (!(0 %in% n)) {
+        cat(t[n],sep="\n")
+      }
+    }
+  } else {
+    if (top != 0) {
+      cat(t[1:top],sep="\n")
+    }
+    cat("...",sep="\n")
+    if (bottom !=0) {
+      bottom <- bottom+1+ln
+      cat(t[bottom:ln],sep="\n")
+    }
+  }
+}
+
+.rround <- function(x,d=2,drop.zero=F) {
+  y <- format(round(as.numeric(x),d),nsmall=d)
+  if (drop.zero) {
+    y <-  sub('^(-)?0\\.','\\1.',y)
+  }
+  y
+}
