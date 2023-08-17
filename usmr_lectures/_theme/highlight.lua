5cd116b0a827fb702e22daf2559e0861a2135304
Martin Corley <Martin.Corley@ed.ac.uk>
2023-08-17 16:35:52 +0100

    ready

--- usmr_lectures/_theme/highlight.lua
+++ usmr_lectures/_theme/highlight.lua
@@ -1,32 +1,46 @@
-function highlight(line_numbers)
-  local highlighter = {
-    Div = function(div)
-      if div.classes:includes('output') then
-        local output_lines = {}
-        for line in string.gmatch(div.text, "[^\r\n]+") do
-          table.insert(output_lines, line)
-        end
-        local highlighted_lines = {}
-        for i, line in ipairs(output_lines) do
-          if line_numbers:find(tostring(i), 1, true) then
-            table.insert(highlighted_lines, '<span class="highlight">' .. line .. '</span>')
-          else
-            table.insert(highlighted_lines, line)
-          end
-        end
-        div.text = table.concat(highlighted_lines, '\n')
-        return div
-      end
-    end
-  }
-  return highlighter
-end
+-- Lua filter for Quarto/Reveal.js to highlight specified line numbers in code chunk output
 
-function Pandoc(doc)
-  if FORMAT == 'revealjs' then
-    local doc_text = pandoc.utils.stringify(doc)
-    local line_numbers = doc.meta["output-line-numbers"]
-    return pandoc.walk_block(pandoc.Div(pandoc.RawBlock('html', doc_text)), highlight(line_numbers))
+-- Function to parse the "output-line-numbers" argument
+local function parseLineNumbers(arg)
+  local lineNumbers = {}
+  for num in string.gmatch(arg, "%d+") do
+    table.insert(lineNumbers, tonumber(num))
   end
+  return lineNumbers
 end
 
+-- Function to generate CSS styles for highlighted line numbers
+local function generateCSS(lineNumbers)
+  local css = ""
+  for _, num in ipairs(lineNumbers) do
+    css = css .. ".highlight-line-" .. num .. "{background-color: yellow;}\n"
+  end
+  return css
+end
+
+-- Function to modify code block output
+local function modifyCodeBlock(block)
+  -- Check if "output-line-numbers" is specified in the code chunk's attributes
+  local attr = block.attributes
+  if attr and attr["output-line-numbers"] then
+    local lineNumbers = parseLineNumbers(attr["output-line-numbers"])
+    local css = generateCSS(lineNumbers)
+    -- Add the generated CSS to the document's header
+    pandoc.utils.stringify(pandoc.Meta{pandoc.MetaInlines{pandoc.RawInline("html", "<style>" .. css .. "</style>")}}, {wrap = false})
+    -- Wrap each output line with a span to apply the highlight
+    for i in ipairs(block.content) do
+      block.content[i] = pandoc.walk_block(block.content[i], {
+        Str = function(el)
+          return pandoc.Span({ pandoc.Str(el.text) }, { class = "highlight-line-" .. i })
+        end
+      })
+    end
+  end
+  return block
+end
+
+return {
+  {
+    CodeBlock = modifyCodeBlock,
+  }
+}
