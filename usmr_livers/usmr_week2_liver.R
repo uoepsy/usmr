b926afe47a5ef399fee55ba5057dc11eb1fc2682
Martin Corley <Martin.Corley@ed.ac.uk>
2023-09-12 14:09:55 +0100

    l2 published

--- usmr_livers/usmr_week2_liver.R
+++ usmr_livers/usmr_week2_liver.R
@@ -1,7 +1,10 @@
+## do sampling, see recordings from 2020
 
-# We've seen in central limit theorem the demonstration of what happens when we repeatedly sample.  
 
-# in practice, we only have one sample. 
+
+# We've seen in central limit theorem the demonstration of what happens when we repeatedly sample.
+
+# in practice, we only have one sample.
 
 movies <- read.csv("https://uoepsy.github.io/data/hollywoodsample2.csv")
 
@@ -19,7 +22,7 @@ sd(movies$RottenTomatoes)/sqrt(nrow(movies))
 
 # we'll see more of this in the readings and exercises, but i'd like to show a little demonstration
 library(tidyverse)
-movies %>% 
+movies %>%
   summarise(
     mRT = mean(RottenTomatoes),
     sRT = sd(RottenTomatoes),
@@ -31,13 +34,13 @@ ggplot(movies, aes(RottenTomatoes)) + geom_histogram(colour='black', binwidth=5)
 
 
 
-## Normal distributions 
+## Normal distributions
 # 68% of values lie within 1 standard deviation of the mean.
 # 95% of values lie within 1.96 standard deviations of the mean.
 # 99.7% of values lie within 3 standard deviations of the mean.
 
 
 # Of the samples of n=50 we *could* take, we would expect
-# 95% of their means to fall between:  
+# 95% of their means to fall between:
 47.04 - (1.96 * 3.69)
 47.04 + (1.96 * 3.69)
