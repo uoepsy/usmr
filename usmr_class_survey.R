394e88520f26b617e5b902e58474281733d31f14
Josiah King <josiahpjking@gmail.com>
2024-10-02 10:31:32 +0100

    fed up with windows

--- usmr_class_survey.R
+++ usmr_class_survey.R
@@ -5,6 +5,8 @@ ff = read_sheet("1LSOjInPdiFbZR2tmoOSWF7-GZhvRThr2eq2n0fJBXJo")
 names(ff)[c(3,5,9,10,14)] <- c("birthmonth","height","eyecolour","catdog","threewords")
 names(ff)[c(4,6,7,8,11,12,13)] <- c("ampm","phone_unlocks","caffeine","caffeine_type","sleeprating","procrastination","multitasking")
 ff <- ff[,c(3:14)]
+ff$procrastination = as.numeric(unlist(ff$procrastination))
+
 ff = ff |> mutate(
   course="usmr",year=2024,
   birthmonth = map(birthmonth, ~which(month.name == .)),
@@ -12,8 +14,10 @@ ff = ff |> mutate(
   eyecolour = tolower(eyecolour),
   catdog = tolower(catdog)
 )
+
+
 df=read_csv("https://uoepsy.github.io/data/surveydata_historical.csv")
 
 ff = ff[,c(names(df)[names(df) %in% names(ff)], names(ff)[!names(ff) %in% names(df)])]
 
-write_csv(ff, "../../data/usmr2024.csv")
+write_csv(ff, file="C:/Users/jking34/Desktop/uoepsy_courses/data/usmr2024.csv")
