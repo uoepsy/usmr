f306eb88d0959cfce1088e0835a05e46e9b46789
Martin Corley <Martin.Corley@ed.ac.uk>
2024-11-01 16:47:17 +0000

    usmr assignment: typo in .Rmd template, and added .R

---
+++ docs/2425/misc/usmr2425rep_template.R
@@ -0,0 +1,28 @@
+# Start by getting your data!
+# IMPORTANT: change the "asymptotic_arias" to be your group name!  
+library(tidyverse)
+source("https://edin.ac/4eYWn7P")
+get_my_data(group_name = "asymptotic_arias")
+
+# after running the above line of code will, the orchestra data should be in your environment
+orchestra
+
+# below here, you can add R code to address the questions in the assignment
+
+### -----------------------------------------------------------------------
+
+### 1. Describe & Explore
+
+
+
+
+
+### 2. Sound & Sentiment
+
+
+
+
+
+### 3. Enjoyment in every note
+
+
