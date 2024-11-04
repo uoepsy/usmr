cbd683f9d5eea2cae322a9ed1e07cd95a6728701
Josiah King <josiahpjking@gmail.com>
2024-11-04 08:43:54 +0000

    data update

--- docs/2425/misc/get_2425_data.R
+++ docs/2425/misc/get_2425_data.R
@@ -1,6 +1,9 @@
 get_my_data <- function(group_name = NULL, individual = FALSE){
   
-  load(url("https://uoepsy.github.io/usmr/2324/misc/pptnames.rdata"))
+  # load(url("https://uoepsy.github.io/usmr/2324/misc/pptnames.rdata"))
+  load("~/Desktop/uoepsy/usmr/docs/2324/misc/pptnames.rdata")
+  infl = c("Sigmund Freud","Beatrix Potter","Stephen Jay Gould")
+  pptnames = pptnames[!pptnames %in% intersect(pptnames, infl)]
   
   groups_2425 = c("asymptotic_arias","bivariate_buglers","chisquare_crooners","data_dancers","error_earworms","f_statistic_funkadelics","gaussian_groovers","histogram_harmonizers","inference_improvisers","jitterplot_jammers","kurtosis_kazoos","logistic_lyricalists","mean_medley_makers","normality_notes","outlier_orchestra","p_value_percussionists","quantile_quartet","standard_deviation_serenaders","t_test_troubadours","y_hat_yodelers")
   
@@ -49,9 +52,9 @@ get_my_data <- function(group_name = NULL, individual = FALSE){
   bs <- c(3, 2, 0, -.7, -6, 3, 3, -.5, .6, 0)
   
   df <- data.frame(
-    init, EVS = xmat %*% bs + rnorm(400,0,2)
+    init, ERS = xmat %*% bs + rnorm(400,0,2)
   )
-  xmatb <- model.matrix(rnorm(400) ~ scale(EVS) + listenermusician + scale(listenerage) + scale(bpm) + family, data = df)
+  xmatb <- model.matrix(rnorm(400) ~ scale(ERS) + listenermusician + scale(listenerage) + scale(bpm) + family, data = df)
   dimnames(xmatb)[[2]]
   bbs <- c(0,2,2,0,0,-6,-1,-2)
   
@@ -74,12 +77,35 @@ get_my_data <- function(group_name = NULL, individual = FALSE){
            table(df$family)['brass'], T)
   
   df$instrument[sample(1:400,1)] <- "Theramin"
+  df$listenerage[sample(1:400,4)] <- -99
+  df$instrument[df$instrument=="Piccolo"] <- 
+    sample(c("Piccollo","Piccolo"), length(df$instrument[df$instrument=="Piccolo"]), T, 
+           prob=c(.1,.9))
+  df$instrument[df$instrument=="French Horn"] <- 
+    sample(c("French Hron","French Horn"), length(df$instrument[df$instrument=="French Horn"]), T, 
+           prob=c(.1,.9))
+  df$listenermusician[1] <- "i don't know"
+
+  inf2 <- tibble(
+    pptname = infl,
+    listenerage = round(runif(3,40,90)),
+    listenermusician = c("0","1","0"),
+    instrument = c("Cello","Timpani","Clarinet"),
+    bpm = c(40,185,50),
+    ERS = c(18.455, 14.341, 15.903),
+    enjoyed = c(0,1,1)
+  )
   
+  df <- bind_rows(df,inf2)
   
-  orchestra <<- df |> 
+  orchestra <<- sample_n(df, nrow(df)) |> 
     transmute(
-      pptname, age=listenerage, musician=listenermusician, instrument,
-      bpm, EVS,enjoyed)
+      pptname, age=listenerage, 
+      musician=ifelse(listenermusician=="0","non-musician",
+                      ifelse(listenermusician=="1","musician",listenermusician)), 
+      instrument,
+      bpm, ERS = round(ERS,2), 
+      enjoyed = ifelse(enjoyed==1,"enjoyed","not_enjoyed"))
 }
 
 
